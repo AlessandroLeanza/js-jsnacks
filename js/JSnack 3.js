54014e7e85cb4be5b8ce69d75188282f85616842
AlessandroLeanza <aleleanza91@live.it>
2024-03-17 09:37:01 +0100

    Esercizio: JSnack 3

---
+++ js/JSnack 3.js
@@ -0,0 +1,15 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+console.log("JSnack 3")
+const numero1 = parseInt(prompt("Inserisci il  primo numero: "))
+const numero2 = parseInt(prompt("Inserisci il  secondo numero: "))
+const numero3 = parseInt(prompt("Inserisci il  terzo numero: "))
+const numero4 = parseInt(prompt("Inserisci il  quarto numero: "))
+const numero5 = parseInt(prompt("Inserisci il  quinto numero: "))
+const numero6 = parseInt(prompt("Inserisci il  sesto numero: "))
+const numero7 = parseInt(prompt("Inserisci il  settimo numero: "))
+const numero8 = parseInt(prompt("Inserisci il  ottavo numero: "))
+const numero9 = parseInt(prompt("Inserisci il  nono numero: "))
+const numero10 = parseInt(prompt("Inserisci il  decimo numero: "))
+console.log("La somma dei numeri é:", numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10)
